c47e85f404bf6878b357289003be4f285e0cef5f
lessfish <lessfish@outlook.com>
2020-01-04 21:07:51 +0800

    add /build

---
+++ 2020/dom-diff-comparaison/build/precache-manifest.5419cc196fa6865442156c24a8dfeef7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87f70c12d2ed3e7d1cf5288913936533",
+    "url": "/index.html"
+  },
+  {
+    "revision": "615543bf5b7fc32ba54a",
+    "url": "/static/js/2.a77bacd0.chunk.js"
+  },
+  {
+    "revision": "c3306c55b29504cf0616",
+    "url": "/static/js/main.6150458a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
